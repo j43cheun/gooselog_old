01251630368824ba373a08c71b96c64a1bfd24b8
Justin Cheung <j43cheun@uwaterloo.ca>
2022-02-21 02:40:35 -0800

    Added Netlify Identity Widget to index page

--- pages/index.vue
+++ pages/index.vue
@@ -29,5 +29,10 @@ export default {
   components: {
     Card,
   },
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
 }
 </script>
